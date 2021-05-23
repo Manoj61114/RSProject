e2ee70a755b8d49cf94d3b627ed128e6a022f18a
Manoj61114 <mannu61114@gmail.com>
2021-05-23 17:29:15 +0530

    For checking purpose

---
+++ src/Java_Interview_Question/PolymorephisnConcept.java
@@ -0,0 +1,10 @@
+package Java_Interview_Question;
+
+public class PolymorephisnConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/OverrideC/SubtractionProgram.java
@@ -0,0 +1,8 @@
+package OverrideC;
+
+public class SubtractionProgram {
+public static void main(String[] args) {
+	System.out.println("gnskhg");
+}
+	
+}
